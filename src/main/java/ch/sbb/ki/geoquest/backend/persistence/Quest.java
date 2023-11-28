01113d83c99a6743ca57c3a546b7581463def2c3
u214578 <florian.hof@sbb.ch>
2023-11-28 14:15:03 +0100

    making persistence work

--- src/main/java/ch/sbb/ki/geoquest/backend/persistence/Quest.java
+++ src/main/java/ch/sbb/ki/geoquest/backend/persistence/Quest.java
@@ -1,10 +1,13 @@
 package ch.sbb.ki.geoquest.backend.persistence;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Entity
 @Data
 public class Quest {
 
+	@Id
+	Long id;
 }
